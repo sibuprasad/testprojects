da4b2dba032ad7c16bdc34e0c06dbf5de4e2795c
sibuprasad <sibuprasad.app@gmail.com>
2024-09-29 16:38:34 +0530

    new change for practice

--- Locator_1.py
+++ Locator_1.py
@@ -7,12 +7,12 @@ service = Service(r"C:\Users\sibup\Downloads\Drivers\chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 driver.maximize_window()
-time.sleep(5)
+# time.sleep(5) wetyujikklk
 
 driver.find_element(By.NAME,"username").send_keys("admin")
 driver.find_element(By.NAME,"password").send_keys("admin123")
 driver.find_element(By.XPATH,"//*[@class = 'oxd-button oxd-button--medium oxd-button--main orangehrm-login-button']").click()
-time.sleep(5)
+# time.sleep(5) vhbhybunji
 
 act_data = driver.title
 exp_data = "OrangeHRM"
